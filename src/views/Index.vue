e4021f9f7d7e1703e9a5846c3e5c52de0f4fa9dd
LSL <you@example.com>
2019-06-19 12:00:25 +0800

    Vue 官网

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,44 +1,48 @@
 <template>
   <div class="index">
-    <div class="banner" :style="styleObj">
-      <div class="container">
-        <div class="banner-text">
-          <div class="slider-info">
-            <a class="details" href="#/" role="button">了解详情 ></a>
-          </div>
-        </div>
+    <div
+      class="banner"
+      v-for="(item,index) in bgUrl"
+      :key="index"
+      :style="{ background: 'url('+ item.banner +') no-repeat center center', backgroundSize: 'cover',height:'680px'}"
+    >
+      <p>{{item.banner}}</p>
+      <div class="banner-text">
+        <a class="details" href="#/">了解详情 ></a>
+        <a class="buy" href="#/">购买 ></a>
       </div>
     </div>
   </div>
 </template>
 <script>
-import png from "@/assets/images/index/bg_xs.png";
-
-// let png =
-// "https://image01.oneplus.cn/shop/201906/13/1798/84063d5fccde396f568fc4e736c621f0.jpg";
 export default {
   name: "index",
   data() {
     return {
-      styleObj: {
-        background: `url(${png}) no-repeat center center`,
-        backgroundSize: "cover",
-        height: "900px"
-      }
+      bgUrl: [
+        {
+          banner: require("@/assets/images/index/banner_1.png")
+        },
+        { banner: require("@/assets/images/index/banner_2.png") }
+      ]
     };
   }
 };
 </script>
 
 <style lang="scss" scoped>
-// .banner {
-//   background: url(../assets/images/index/bg_xs.png) no-repeat center;
-//   -webkit-background-size: cover;
-//   -moz-background-size: cover;
-//   -o-background-size: cover;
-//   -ms-background-size: cover;
-//   background-size: cover;
-//   // height: 900px;
-//   /* position: relative; */
-// }
+.banner {
+  .banner-text {
+    width: 100%;
+    text-align: center;
+    .details {
+      display: inline-flex;
+      padding-top: 200px;
+    }
+
+    .buy {
+      margin-left: 30px;
+    }
+  }
+}
 </style>
