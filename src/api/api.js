3b4b8b799ebdc5ad398d95fa2581f69f59e4d843
lsl <273974052@qq.com>
2019-06-22 10:08:06 +0800

    vue 官网

--- src/api/api.js
+++ src/api/api.js
@@ -1,9 +1,21 @@
 import config from '@/config/config'
+// console.log(config);
+// console.log(config.url);
 
-console.log(config);
-console.log(config.url);
-const register = config.url + '/user/GetVerifycode' //注册页验证码获取
-console.log(register);
+
+
+
+
+
+
+const register = config.url + '/user/GetVerifycode' //注册页验证码获取，GET请求
+const isRegister = config.url + '/user/IsMobileRegist' //判断手机号码是否已经注册，GET请求
+const passwordLogin = config.url + '/user/LoginByPass' //用户登录，(手机号码和密码)，GET请求
+const verifycodeLogin = config.url + '/user/LoginByVerifycode' //用户登录，(手机号码和验证码)，GET请求
+// console.log(register);
 export default {
     register,
+    isRegister,
+    passwordLogin,
+    verifycodeLogin
 }
--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,6 @@
-import Vue from 'vue';
 const production = true; //是否为生产环境
 const url = production ? 'http://39.98.167.238:8096' : 'http://192.168.1.9:8096'; //请求地址
 console.log(url);
-// Vue.prototype.$url = url;
 export default {
   url //请求地址
   // static: `${url}/static/img`, //静态资源路径
